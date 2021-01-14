61b339b3f3c33787d2cf269fc783781ae4e53fc8
Sasi Patterson <sasimapatterson@gmail.com>
2021-01-14 19:53:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Write your code below this line 👇
+import math
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+
+def paint_calc(height, width, cover):
+  area = height * width
+  num_of_cans = math.ceil(area / cover)
+  print(f"You'll need {num_of_cans} cans of paint")
+
+  
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
